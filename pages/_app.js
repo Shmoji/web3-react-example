17fa82a25eaadb01d8bc30b0a9e680cfb3967d9b
Shmoji <jackj_skate@hotmail.com>
2021-08-09 22:09:29 -0400

    here's the code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import { Web3ReactProvider } from '@web3-react/core'
+import Web3 from 'web3'
+
+function getLibrary(provider) {
+  return new Web3(provider)
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
